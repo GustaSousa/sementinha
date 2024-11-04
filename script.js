d019f5aeda4b0fcf25f2add2973aa22d24b9b001
Gustavo Sousa <ogustavo.sousa@icloud.com>
2024-11-04 19:25:36 -0300

    Instalaçoes

--- script.js
+++ script.js
@@ -116,3 +116,26 @@
     // Adiciona o observer à seção
     const section = document.querySelector('#voce-sabia');
     observer.observe(section);
+
+//INSTALASÇOES
+
+    const modal = document.getElementById('modal');
+    const modalImage = document.getElementById('modal-image');
+    const closeBtn = document.getElementById('close');
+
+    document.querySelectorAll('.galeria-item').forEach(image => {
+        image.addEventListener('click', () => {
+            modal.style.display = 'flex';
+            modalImage.src = image.src;
+        });
+    });
+
+    closeBtn.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
